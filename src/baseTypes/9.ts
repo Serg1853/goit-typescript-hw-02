b191a1138d9ec6c9bf31b6f8803c89dc317e8335
Serg1853 <sergey24226@gmail.com>
2023-12-06 13:28:14 +0200

    error

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -12,7 +12,7 @@ interface Page {
   };
 }
 
-const page1 = {
+const page1: Page = {
   title: "The awesome page",
   likes: 100,
   accounts: ["Max", "Anton", "Nikita"],
@@ -23,7 +23,7 @@ const page1 = {
   },
 };
 
-const page2 = {
+const page2: Page = {
   title: "Python or Js",
   likes: 5,
   accounts: ["Alex"],
